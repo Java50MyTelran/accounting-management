dd4396040f8b117d0a9a038de3450fb8a35db4bf
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-29 08:57:33 +0300

    accounting-management

--- src/main/java/telran/security/accounting/repo/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package telran.security.accounting.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import telran.security.accounting.model.Account;
-
-public interface AccountRepo extends MongoRepository<Account, String> {
-
-}
--- src/main/java/telran/security/accounting/service/AccountingServiceImpl.java
+++ src/main/java/telran/security/accounting/service/AccountingServiceImpl.java
@@ -1,47 +1,50 @@
 package telran.security.accounting.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import com.mongodb.client.result.DeleteResult;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.exceptions.NotFoundException;
 import telran.security.accounting.dto.AccountDto;
+import telran.security.accounting.exceptions.AccountNotFoundException;
+import telran.security.accounting.exceptions.AccountStateException;
 import telran.security.accounting.model.Account;
-import telran.security.accounting.repo.AccountRepo;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AccountingServiceImpl implements AccountingService{
-	
-final AccountRepo accountRepo;
-final PasswordEncoder passwordEncoder;
+	final MongoTemplate mongoTemplate;
 
 	@Override
 	public AccountDto addAccount(AccountDto accountDto) {
 		String email = accountDto.email();
-		AccountDto accountEncoded = getAccountDtoEncoded(accountDto);
-		if(accountRepo.existsById(email)) {
-			throw new IllegalStateException(String.format("account %s already exists", email));
+		Account account = null;
+		try {
+			account = mongoTemplate.insert(Account.of(accountDto));
+		} catch (DuplicateKeyException e) {
+			throw new AccountStateException(email);
 		}
-		Account account = accountRepo.save(Account.of(accountEncoded));
-		log.debug("account {} has been saved", email);
+		log.debug("account {} has been saved",email);
 		return account.build();
 	}
 
-	private AccountDto getAccountDtoEncoded(AccountDto accountDto) {
-		
-		return new AccountDto(accountDto.email(),
-				passwordEncoder.encode(accountDto.password()), accountDto.roles());
-	}
+	
 
 	@Override
 	public AccountDto removeAccount(String email) {
-		Account account = accountRepo.findById(email)
-				.orElseThrow(()->new NotFoundException(String.format("Account %s doesn't exist", email)));
-		accountRepo.delete(account);
+		Account account = mongoTemplate.findAndRemove(new Query(Criteria.where("email")
+				.is(email)), Account.class);
+		if(account == null) {
+			throw new AccountNotFoundException(email);
+		}
 		log.debug("account {} has been removed", email);
 		return account.build();
 	}
